64679395a7c117f3698dbbc2ba7f40fb924f49c5
Trevor Templin <c.trevor.templin@gmail.com>
2022-03-16 09:52:37 -0600

    building first query route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // external requirements and baseline declarations
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 
 // port config
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,21 @@ const PORT = process.env.PORT || 3001;
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to mysql db
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'mysqldummy',
+        password: 'mysqldummypassword',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // routing content
 
